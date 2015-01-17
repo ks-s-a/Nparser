cfb728e52af9fa16264d07437c5d7992ee64e508
ks-s-a <ksenofontovsa@yandex.ru>
2015-01-17 18:35:34 +0300

    finish some work

--- googlelinks.js
+++ googlelinks.js
@@ -1,39 +1,48 @@
 var casper = require('casper').create();
+var siteLink = casper.cli.get(0);
+
+if (!siteLink || siteLink.indexOf('.') === -1)
+  casper.die('Wrong site link!');
 
 //casper.userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36');
 
 casper.start('http://google.ru/', function() {
   // search for target from google form
-  this.fill('form[action="/search"]', { q: 'site:take365.org' }, true);
+  this.fill('form[action="/search"]', { q: 'site:' + siteLink }, true);
 });
 
-casper.then(checkNextPageAndClick);
+casper.then(parsePageAndClickToNext);
 
 casper.run();
 
 function getLinks() {
-    var searchLinks = document.querySelectorAll('h3.r a');
-    var response = {};
-
-    //response.isLastSearchPage = !document.querySelectorAll('#nav td.b:last-child > a');
-    response.data = Array.prototype.map.call(searchLinks, function(e) {
-        return e.getAttribute('href');
-    });
-
-    return response;
+  var result = {};
+  var linksElements = document.querySelectorAll('h3.r a');
+  
+  // Looking for next page button.
+  result.haveMorePages = document.querySelectorAll('#nav td.b:last-child > a').length;
+  
+  // Extract links from the elements.
+  result.searchLinks = Array.prototype.map.call(linksElements, function(e) {
+      return e.getAttribute('href').slice(7).split('&')[0];
+  });
+  
+  return result;
 }
 
-function evalAndAddLinks(ctx) {
+function evalAndShowLinks(ctx) {
   var res = ctx.evaluate(getLinks);
 
-  res.data.forEach(function(v) { console.log( v.slice(7).split('&')[0] ) });
+  res.searchLinks.forEach(function(v) { console.log(v) });
 
-  //return res.isLastSearchPage;
+  return res.haveMorePages;
 }
 
-function checkNextPageAndClick() {
-  var islastPage = evalAndAddLinks(this);
-
-  //if (!islastPage)
-    this.thenClick('#nav td.b:last-child > a', checkNextPageAndClick);
+function parsePageAndClickToNext() {
+  if (!evalAndShowLinks(this))
+    return;
+  
+  casper.wait('2000', function() {
+    casper.thenClick('#nav td.b:last-child > a', parsePageAndClickToNext);
+  });
 }
